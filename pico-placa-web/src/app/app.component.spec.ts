d846039c0efd21c5ac701bc56f21b30f63964ac6
fredrom <wfrediromans@outlook.com>
2020-06-21 13:10:24 -0500

    MAKING THE FIRST TWO UNIT TESTS

--- pico-placa-web/src/app/app.component.spec.ts
+++ pico-placa-web/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -16,16 +17,21 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pico-placa-web'`, () => {
+  /**
+   * CUSTOM TEST PROCESS
+   * AUTHOR: FREDI ROMAN
+   */
+  
+  it(`should have a title 'Pico y Placa restriction for Quito city'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pico-placa-web');
+    expect(app.title).toEqual('Pico y Placa restriction for Quito city');
   });
 
-  it('should render title', () => {
+  it('should render the title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pico-placa-web app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Pico y Placa restriction for Quito city');
   });
 });
--- pico-placa-web/src/app/app.component.ts
+++ pico-placa-web/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pico-placa-web';
+  public title: string;
+
+  constructor() {
+    this.title = 'Pico y Placa restriction for Quito city';
+  }
+
 }
