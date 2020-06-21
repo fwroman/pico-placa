3b1fda9c2c315ff48260cd6d04368f4bad2bfe4f
fredrom <wfrediromans@outlook.com>
2020-06-21 13:42:33 -0500

    - MAKING THE SECOND BUNCH OF UNIT TESTS IN THE APP COMPONENT - CORRECTING THE EXTENSION OF THE STYLE SHEET FILE FROM style.css TO style.scss IN angular.json FILE

--- pico-placa-web/src/app/app.component.spec.ts
+++ pico-placa-web/src/app/app.component.spec.ts
@@ -21,17 +21,56 @@ describe('AppComponent', () => {
    * CUSTOM TEST PROCESS
    * AUTHOR: FREDI ROMAN
    */
-  
+  it('should have div with .container class', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement;
+    expect(compiled.query(By.css('.container'))).toBeTruthy();
+  });
+
   it(`should have a title 'Pico y Placa restriction for Quito city'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Pico y Placa restriction for Quito city');
   });
 
-  it('should render the title', () => {
+  it('should render the title with h2 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Pico y Placa restriction for Quito city');
+    expect(compiled.querySelector('h2').textContent).toContain('Pico y Placa restriction for Quito city');
+  });
+
+  it('should render hr tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('hr')).toBeTruthy();
+  });
+
+  it('should have h2 and hr tags inside tag with .container class', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement;
+    const containerTag = compiled.query(By.css('.container')).nativeElement;
+    expect(containerTag.querySelector('h2')).toBeTruthy();
+    expect(containerTag.querySelector('hr')).toBeTruthy();
+  });
+
+  it('DOM element with class .container should have tag with .col-12 class inside of it', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement;
+    const containerTag = compiled.query(By.css('.container')).nativeElement;
+    expect(containerTag.querySelector('.col-12')).toBeTruthy();
+  });
+
+  it('DOM element with class .col-12 should have child component with tag pico-placa', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement;
+    const containerTag = compiled.query(By.css('.container')).nativeElement;
+    const col12Tag = compiled.query(By.css('.col-12')).nativeElement;
+    expect(containerTag.querySelector('pico-placa')).toBeTruthy();
   });
 });
