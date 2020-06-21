a40dde4e33b14ad577130cb2228270e885d325c9
fredrom <wfrediromans@outlook.com>
2020-06-21 10:59:13 -0500

    CREATING THE PICO-PLACA COMPONENT FOR SIMULATING QUITO'S 'PICO Y PLACA' BEHAVIOUR

---
+++ pico-placa-web/src/app/components/pico-placa/pico-placa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pico-placa',
+  templateUrl: './pico-placa.component.html',
+  styleUrls: ['./pico-placa.component.scss']
+})
+export class PicoPlacaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
